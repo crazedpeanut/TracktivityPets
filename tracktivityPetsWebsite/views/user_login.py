4aaf2bb7be13ea32a4d9c33174c9f646a04421cc
john@bitlink.com.au <john@bitlink.com.au>
2015-05-01 14:22:48 +1000

    Added login template.
    Separated splash and login templates.

--- tracktivityPetsWebsite/views/user_login.py
+++ tracktivityPetsWebsite/views/user_login.py
@@ -14,12 +14,12 @@ def user_login(request):
         try:
             username = request.POST['username'] #get and store username
         except:
-            return render(request, 'tracktivityPetsWebsite/login.html', { "error_message": "No username" })
+            return render(request, 'tracktivityPetsWebsite/splash.html', { "error_message": "No username" })
         
         try:
             password = request.POST['password'] #get and store password
         except:
-            return render(request, 'tracktivityPetsWebsite/login.html', { "error_message": "No password" })
+            return render(request, 'tracktivityPetsWebsite/splash.html', { "error_message": "No password" })
         
         user = authenticate(username=username, password=password) #django method to see if user exists in database
         if user is not None:
@@ -28,14 +28,14 @@ def user_login(request):
                 fitbit_synched = False;
                 if fitapp.utils.is_integrated(user):
                     fitbit_synched = True
-                return render(request, 'tracktivityPetsWebsite/login.html', {"synched": fitbit_synched})
+                return render(request, 'tracktivityPetsWebsite/splash.html', {"synched": fitbit_synched})
             else:
                 fitbit_synched = False;
                 if fitapp.utils.is_integrated(user):
                     fitbit_synched = True
-                return render(request, 'tracktivityPetsWebsite/login.html')
+                return render(request, 'tracktivityPetsWebsite/splash.html')
         else:
-            return render(request, 'tracktivityPetsWebsite/login.html', { "error_message": "Incorrect username/password combination" }) #no user was found
+            return render(request, 'tracktivityPetsWebsite/splash.html', { "error_message": "Incorrect username/password combination" }) #no user was found
         
     else:
-        return render(request, 'tracktivityPetsWebsite/login.html')
+        return render(request, 'tracktivityPetsWebsite/splash.html')
