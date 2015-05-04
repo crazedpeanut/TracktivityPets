d4339deb2a9d4afebd1a9f8c33e85020986157b1
John Kendall <john@johnkendall.net>
2015-05-04 17:02:44 +1000

    Update register.py

--- tracktivityPetsWebsite/views/register.py
+++ tracktivityPetsWebsite/views/register.py
@@ -3,9 +3,6 @@ from django.http import HttpResponse
 #from tracktivityPetsWebsite import utils #example of using utils
 
 def register(request):
-<<<<<<< HEAD
     #message = utils.register_user('bob', 'brown', 'bob@test.com', 'bobby', 'swagcats', 'swagcats') #example of using register function, message is None if it worked
     pass
-=======
     return render(request, 'tracktivityPetsWebsite/register.html')
->>>>>>> origin/master
