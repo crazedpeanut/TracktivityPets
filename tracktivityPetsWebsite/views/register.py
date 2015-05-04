45acd73f240c3ca213e03d4c33e00a63c8657d10
crazedpeanut <john@bitlink.com.au>
2015-05-04 19:37:12 +1000

    Fix register view

--- tracktivityPetsWebsite/views/register.py
+++ tracktivityPetsWebsite/views/register.py
@@ -4,5 +4,4 @@ from django.http import HttpResponse
 
 def register(request):
     #message = utils.register_user('bob', 'brown', 'bob@test.com', 'bobby', 'swagcats', 'swagcats') #example of using register function, message is None if it worked
-    pass
     return render(request, 'tracktivityPetsWebsite/register.html')
