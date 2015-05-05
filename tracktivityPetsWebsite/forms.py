33026fce03b91c7bfc82f51ce3d569236c246fb1
crazedpeanut <john@bitlink.com.au>
2015-05-05 10:32:50 +1000

    Replaced static form in login.html with a special one.

--- tracktivityPetsWebsite/forms.py
+++ tracktivityPetsWebsite/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class LoginForm(Forms.form):
-    email = forms.CharField(label="Email", max_length=100, widget=forms.EmailInput)
-    password = form.CharField(label="Password", max_length=100, widget=forms.PasswordInput)
-    rememberMe = forms.BooleanField(label="Remember me", widget=forms.CheckBoxInput)
+class LoginForm(forms.Form):
+    email = forms.CharField(max_length=100, widget=forms.EmailInput(attrs={'placeholder':'Email', 'class':'form-control', "required":"", "autofocus":""}))
+    password = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'placeholder':'Password', 'class':'form-control', "required":""}))
+    rememberMe = forms.BooleanField(label="Remember me", required=False, initial=False)
--- tracktivityPetsWebsite/views/user_login.py
+++ tracktivityPetsWebsite/views/user_login.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.conf import settings
+from tracktivityPetsWebsite.forms import LoginForm
 import fitapp
 
 def user_login(request):
@@ -12,7 +13,48 @@ def user_login(request):
             fitbit_synched = True
         return render(request, 'tracktivityPetsWebsite/login.html', {"synched": fitbit_synched})
     
-    elif request.method == 'POST': #if http request was made with POST type
+    elif(request.method == "GET"):
+        loginForm = LoginForm()
+        return render(request, 'tracktivityPetsWebsite/splash.html', {'loginForm': loginForm} )
+    
+    elif(request.method == "POST"): #if http request was made with POST type
+        
+        loginForm = LoginForm(request.POST)
+        
+        if(loginForm.is_valid() is not True):
+            loginForm = LoginForm()
+            return render(request, 'tracktivityPetsWebsite/splash.html', { "error_message": "Form invalid",'loginForm':loginForm})
+        else:
+            email = loginForm.cleaned_data['email']
+            password = loginForm.cleaned_data['password']
+            rememberMe = loginForm.cleaned_data['rememberMe']
+            u = User.objects.get(email=email)
+            
+            user = authenticate(username=u.get_username(), password=password)
+            
+            if(user is not None):
+                if(user.is_active):
+                    login(request, user)
+                    fitbit_synched = False
+                    
+                    if(fitapp.utils.is_integrated(user)):
+                        fitbit_synched = True
+                        
+                    if(rememberMe):
+                        request.session.set_expiry(settings.REMEMBER_ME_DURATION)
+                    return render(request, 'tracktivityPetsWebsite/splash.html', {"synched": fitbit_synched})
+                else:
+                    fitbit_synched = False
+                    
+                    if(fitapp.utils.is_integrated(user)):
+                        fitbit_synched = True
+                        return render(request, 'tracktivityPetsWebsite/splash.html')
+            else:
+                loginForm = LoginForm()
+                return render(request, 'tracktivityPetsWebsite/splash.html', { "error_message": "Incorrect username/password combination",'loginForm':loginForm}) #no user was found
+                
+        
+        '''
         try:
             email = request.POST['email'] #get and store username
         except:
@@ -27,6 +69,7 @@ def user_login(request):
             u = User.objects.get(email=email)
         except:
             return render(request, 'tracktivityPetsWebsite/splash.html', { "error_message": "No user with that email" })
+        
         
         user = authenticate(username=u.get_username(), password=password) #django method to see if user exists in database
         if user is not None:
@@ -46,7 +89,10 @@ def user_login(request):
                     fitbit_synched = True
                 return render(request, 'tracktivityPetsWebsite/splash.html')
         else:
-            return render(request, 'tracktivityPetsWebsite/splash.html', { "error_message": "Incorrect username/password combination" }) #no user was found
+            loginForm = LoginForm()
+            return render(request, 'tracktivityPetsWebsite/splash.html', { "error_message": "Incorrect username/password combination",'loginForm':loginForm}) #no user was found
         
     else:
-        return render(request, 'tracktivityPetsWebsite/splash.html')
+        loginForm = LoginForm()
+        return render(request, 'tracktivityPetsWebsite/splash.html', {'loginForm': loginForm} )
+    '''
