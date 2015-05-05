1cff15349f2623d5732d4863f73fb67dce87eba0
crazedpeanut <john@bitlink.com.au>
2015-05-05 10:39:39 +1000

    Added redirection to splash page after logout

--- tracktivityPetsWebsite/views/user_logout.py
+++ tracktivityPetsWebsite/views/user_logout.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
+from django.core.urlresolvers import reverse
     
 def user_logout(request):
     if request.user.is_authenticated(): #if user is logged in
         logout(request) #log them out
-        return HttpResponse("Logged out")
-    else:
-        return HttpResponse("No user to logout")
         
+    return HttpResponseRedirect(reverse('tracktivityPetsWebsite:user_login'))
