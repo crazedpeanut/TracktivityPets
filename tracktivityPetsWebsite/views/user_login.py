12b27502d79d2bbed568eb59b88b23f79decb0c8
andrew-jones-au <andrew-jones@mail.com>
2015-05-07 20:59:17 +1000

    Minor code changes. Registered users now signed in and taken to pet selection page. remove code redundency. other small changes.

--- tracktivityPetsWebsite/views/user_login.py
+++ tracktivityPetsWebsite/views/user_login.py
@@ -9,20 +9,17 @@ import fitapp
 
 def user_login(request):
     if  request.user.is_authenticated(): #if user is logged in
-        fitbit_synched = False;
-        if fitapp.utils.is_integrated(request.user):
-            fitbit_synched = True
         return redirect('tracktivityPetsWebsite:dashboard') #go to dashboard
     
-    elif(request.method == "GET"):
+    elif request.method == "GET":
         loginForm = LoginForm()
         return render(request, 'tracktivityPetsWebsite/splash.html', {'loginForm': loginForm} )
     
-    elif(request.method == "POST"): #if http request was made with POST type
+    elif request.method == "POST": #if http request was made with POST type
         
         loginForm = LoginForm(request.POST)
         
-        if(loginForm.is_valid() is not True):
+        if loginForm.is_valid() is not True:
             loginForm = LoginForm()
             return render(request, 'tracktivityPetsWebsite/splash.html', { "error_message": "Form invalid",'loginForm':loginForm})
         else:
@@ -33,67 +30,24 @@ def user_login(request):
             
             user = authenticate(username=u.get_username(), password=password)
             
-            if(user is not None):
-                if(user.is_active):
+            if user is not None:
+                if user.is_active:
                     login(request, user)
                     fitbit_synched = False
                     
-                    if(fitapp.utils.is_integrated(user)):
+                    if fitapp.utils.is_integrated(user):
                         fitbit_synched = True
                         
-                    if(rememberMe):
+                    if rememberMe:
                         request.session.set_expiry(settings.REMEMBER_ME_DURATION)
                     return redirect('tracktivityPetsWebsite:dashboard')#return render(request, 'tracktivityPetsWebsite/splash.html', {"synched": fitbit_synched})
                 else:
                     fitbit_synched = False
                     
-                    if(fitapp.utils.is_integrated(user)):
+                    if fitapp.utils.is_integrated(user):
                         fitbit_synched = True
                         return render(request, 'tracktivityPetsWebsite/splash.html')
             else:
                 loginForm = LoginForm()
                 return render(request, 'tracktivityPetsWebsite/splash.html', { "error_message": "Incorrect username/password combination",'loginForm':loginForm}) #no user was found
                 
-        
-        '''
-        try:
-            email = request.POST['email'] #get and store username
-        except:
-            return render(request, 'tracktivityPetsWebsite/splash.html', { "error_message": "No email" })
-        
-        try:
-            password = request.POST['password'] #get and store password
-        except:
-            return render(request, 'tracktivityPetsWebsite/splash.html', { "error_message": "No password" })
-        
-        try:
-            u = User.objects.get(email=email)
-        except:
-            return render(request, 'tracktivityPetsWebsite/splash.html', { "error_message": "No user with that email" })
-        
-        
-        user = authenticate(username=u.get_username(), password=password) #django method to see if user exists in database
-        if user is not None:
-            if user.is_active: #if the account is activated
-                login(request, user) #log the user into a session
-                fitbit_synched = False;
-                if fitapp.utils.is_integrated(user):
-                    fitbit_synched = True
-                    
-                if request.POST.get('remember-me', False):
-                    request.session.set_expiry(settings.REMEMBER_ME_DURATION)
-                    
-                return render(request, 'tracktivityPetsWebsite/splash.html', {"synched": fitbit_synched})
-            else:
-                fitbit_synched = False;
-                if fitapp.utils.is_integrated(user):
-                    fitbit_synched = True
-                return render(request, 'tracktivityPetsWebsite/splash.html')
-        else:
-            loginForm = LoginForm()
-            return render(request, 'tracktivityPetsWebsite/splash.html', { "error_message": "Incorrect username/password combination",'loginForm':loginForm}) #no user was found
-        
-    else:
-        loginForm = LoginForm()
-        return render(request, 'tracktivityPetsWebsite/splash.html', {'loginForm': loginForm} )
-    '''
