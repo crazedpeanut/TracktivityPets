e1c9131ad71b796dd5f70025ffc07d303cd160b6
andrew-jones-au <andrew-jones@mail.com>
2015-05-10 22:07:36 +1000

    Modified django-fitbit to better suit our needs for if a user disallows our site to access their data, but returns later.

--- tracktivityPetsWebsite/views/dashboard.py
+++ tracktivityPetsWebsite/views/dashboard.py
@@ -5,19 +5,22 @@ from django.templatetags.static import static
 import fitapp
 from tracktivityPetsWebsite import utils
 from django.shortcuts import redirect
+import fitapp.utils
 import json
 
 @login_required
 def dashboard(request):
     
-    if not utils.is_fitbit_linked(request.user):
+    if not utils.is_fitbit_linked(request.user) or not fitapp.utils.is_integrated(request.user):
         return redirect('/fitbit/login')
+    
     elif request.user.profile.current_pet is None:#take them to the page to select a pet
         return redirect('tracktivityPetsWebsite:pet_selection')
         
     success, data = utils.update_user_fitbit(request)
     
-    fitbit_synched = utils.is_fitbit_linked(request.user)
+    if not success and data == 103:# :103: Fitbit authentication credentials are invalid and have been removed.
+        return redirect('/fitbit/login')
         
     start_url = static('tracktivityPetsWebsite/images')
     current_mood = request.user.profile.current_pet.get_current_mood()
@@ -32,15 +35,14 @@ def dashboard(request):
     
     age = request.user.profile.current_pet.get_age_in_days()
     
+    error = ""
+    
     if not success:
+        error = data
+        
         data = {}
         data['experience_gained'] = -1
         data['levels_gained'] = -1
-    else:   
-        if not 'experience_gained' in data:
-            data['experience_gained'] = -1
-        if not 'levels_gained' in data:
-            data['levels_gained'] = -1
         
     happiness_data = request.user.profile.current_pet.get_happiness_last_seven_days()#[25, 50, 40, 70, 10, 80, 60]#temp data
     largest_experience, experience_data = request.user.profile.current_pet.get_experience_last_seven_days()#[2500, 5000, 4000, 7000, 1000, 8000, 6000]
@@ -53,7 +55,6 @@ def dashboard(request):
     return render(request, 'tracktivityPetsWebsite/dashboard.html',  
                   {
                    "pet_name": pet_name,
-                   "synched": fitbit_synched,
                    "happiness_graph_data": happiness_data,
                    "experience_graph_data": experience_data,
                    "happiness_today": happiness_today,
@@ -62,5 +63,6 @@ def dashboard(request):
                    "level_data": level_data,
                    "age": age,
                    "experience_gained": data['experience_gained'],
-                   "levels_gained": data['levels_gained']
+                   "levels_gained": data['levels_gained'],
+                   "error": error
                    })
