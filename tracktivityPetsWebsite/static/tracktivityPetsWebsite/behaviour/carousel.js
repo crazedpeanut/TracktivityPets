31fcd7cee2939b546d88ed5e933508012c2843bb
yatesrates <yatesrates@gmail.com>
2015-05-11 17:35:07 +1000

    first carousel working, second still WIP

---
+++ tracktivityPetsWebsite/static/tracktivityPetsWebsite/behaviour/carousel.js
@@ -0,0 +1,22 @@
+$(function() {
+	$('#carousel').carouFredSel({
+		width:'100%',
+		items: 3,
+//		items: {
+//			visible: 1,
+//			start:1
+//		},
+		auto: false,
+		scroll: {
+			items: 1,
+			duration: 1000,
+			timeoutDuration: 3000
+		},
+		prev: '#prev',
+		next: '#next',
+//		pagination: {
+//			container:'#pager',
+//			deviation: 1
+//		}
+	});
+});
