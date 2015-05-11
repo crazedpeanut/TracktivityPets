723d1925cdd284b93648b47b2e3c7908d8ea93d7
andrew-jones-au <andrew-jones@mail.com>
2015-05-11 22:44:52 +1000

    Updated feedback view and template to send an email, note that it needs an SMTP server which is yet to be set up, then gets defined in settings.py

--- tracktivityPetsWebsite/views/feedback.py
+++ tracktivityPetsWebsite/views/feedback.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.mail import send_mail
 
 
 def feedback(request):
+    
+    if request.method == "POST":
+        contents = request.POST.get("contents", None)
+        
+        if contents is None or contents is "":
+            return render(request, 'tracktivityPetsWebsite/feedback.html')
+        
+        username = request.user.get_username()
+        user_email = request.user.email
+        email_to = "g3325629@trbvm.com"
+        subject = "Tracktivity Pets Feedback from " + username
+        
+        send_mail(subject, contents, user_email, [email_to], fail_silently=False)
+        return HttpResponse("submitted successfully")
     return render(request, 'tracktivityPetsWebsite/feedback.html')
 
 
