0f61d126f6f17e15c80dc7322c331994d40d68e2
john@bitlink.com.au <john@bitlink.com.au>
2015-07-23 15:04:09 +1000

    Added logging to fitapp.views.update

--- fitapp/views.py
+++ fitapp/views.py
@@ -35,7 +35,7 @@ import hashlib, binascii
 from django.conf import settings
 
 logger = logging.getLogger(__name__)
-hdlr = logging.FileHandler('/var/tmp/celerylog.log')
+hdlr = logging.FileHandler('/var/tmp/tracktivitypets_fitapp.log')
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
 logger.addHandler(hdlr) 
