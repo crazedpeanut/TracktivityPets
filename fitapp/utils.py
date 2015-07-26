f1cc9ff0a024485bdd2823b1b8b5b158fa228bdc
john@bitlink.com.au <john@bitlink.com.au>
2015-07-26 20:24:11 +1000

    Swag

--- fitapp/utils.py
+++ fitapp/utils.py
@@ -8,6 +8,7 @@ from . import defaults
 from .models import UserFitbit
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 hdlr = logging.FileHandler('./tracktivitypets_fitapp_utils.log')
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
