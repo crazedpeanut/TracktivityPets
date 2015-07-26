43f88b0544ad59131a265d39221804d176937fe3
john@bitlink.com.au <john@bitlink.com.au>
2015-07-26 20:38:52 +1000

    Test

--- fitapp/utils.py
+++ fitapp/utils.py
@@ -73,11 +73,12 @@ def get_fitbit_data(fbuser, resource_type, base_date=None, period=None,
     resource_path = resource_type.path()
     
     logger.debug("Detail level: %s, startTime: %s, endTime: %s", period, str(base_date), str(end_date))
+ 
+    return ("Detail level: %s, startTime: %s, endTime: %s" % (period, str(base_date), str(end_date)))
     
     if('15min' in period or '1min' in period):#untested!!!!!
         data = fb.intraday_time_series(resource_path, user_id=fbuser.fitbit_user,
                       detail_level=period, start_time=base_date, end_time=end_date)
-        return ("Detail level: %s, startTime: %s, endTime: %s" % (period, str(base_date), str(end_date)))
     else:
         data = fb.time_series(resource_path, user_id=fbuser.fitbit_user,
                               period=period, base_date=base_date,
