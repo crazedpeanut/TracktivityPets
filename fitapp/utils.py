a0f4325f76492052df7fc2adfb6642fee06b4007
john@bitlink.com.au <john@bitlink.com.au>
2015-07-27 16:42:43 +1000

    Test

--- fitapp/utils.py
+++ fitapp/utils.py
@@ -73,13 +73,13 @@ def get_fitbit_data(fbuser, resource_type, base_date=None, period=None,
     resource_path = resource_type.path()
     
     logger.debug("Detail level: %s, startTime: %s, endTime: %s", period, str(base_date), str(end_date))
-
+    '''
     data = fb.intraday_time_series(resource_path, start_time=base_date, end_time=end_date)
     '''
     data = fb.time_series(resource_path, user_id=fbuser.fitbit_user,
                               period=period, base_date=base_date,
                               end_date=end_date)
-    '''
+
 
     if('HTTP Error 500' in data[resource_path.replace('/', '-')]):
         logger.error('HTTPServerError - >=500 - Fitbit server error or maintenance.')
