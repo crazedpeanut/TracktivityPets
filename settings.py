8c5bb92df4be606796f2828c3658a505efcda375
john@bitlink.com.au <john@bitlink.com.au>
2015-08-28 14:40:37 +1000

    Swag

--- settings.py
+++ settings.py
@@ -69,8 +69,12 @@ WSGI_APPLICATION = 'TracktivityPets.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tracktivitypets',
+        'USER': 'tracktivitypets',
+        'PASSWORD': 'tracktivitypets',
+        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
