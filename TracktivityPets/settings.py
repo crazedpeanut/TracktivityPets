3bbccc690b5dc7e4a62d8f3d0929e00827fc4137
john@bitlink.com.au <john@bitlink.com.au>
2015-08-28 14:40:12 +1000

    Yolo

--- TracktivityPets/settings.py
+++ TracktivityPets/settings.py
@@ -69,11 +69,6 @@ WSGI_APPLICATION = 'TracktivityPets.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
- '''   'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }'''
-
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tracktivitypets',
