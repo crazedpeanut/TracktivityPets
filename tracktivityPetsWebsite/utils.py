a651768a689fc9b6aee36aeb71916ce7079b7f46
john@bitlink.com.au <john@bitlink.com.au>
2015-09-02 16:36:29 +1000

    Yo

--- tracktivityPetsWebsite/utils.py
+++ tracktivityPetsWebsite/utils.py
@@ -333,7 +333,7 @@ def update_user_challenges(user):
 
             steps = 0
 
-            for date in steps_during_json:
+            for date in steps_during_json['objects']:
                 steps += date['value']
                 logger.debug("Updating state steps from: %d to %d" % (uc.state.state.steps, uc.state.state.steps + steps))
                 uc.state.state.steps = steps
