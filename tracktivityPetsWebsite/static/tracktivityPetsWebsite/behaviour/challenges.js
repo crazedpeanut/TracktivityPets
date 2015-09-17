90f8e50a63ab0b2403ab46792cf1bfc936a287ee
john@bitlink.com.au <john@bitlink.com.au>
2015-09-17 20:18:40 +1000

    Yo

--- tracktivityPetsWebsite/static/tracktivityPetsWebsite/behaviour/challenges.js
+++ tracktivityPetsWebsite/static/tracktivityPetsWebsite/behaviour/challenges.js
@@ -119,8 +119,6 @@ function get_active_challenge_details(challenge)
             $("#challenges_detail_modal").find(".challenge_detail_description").html(data['challenge']['overview']);
             $("#challenges_detail_modal").find(".challenge_detail_header").html(data['challenge']['name']);
 
-            alert();
-
             $("#current_challenge_rewards_table").html("");
             $("#challenges_detail_modal").find(".available_challenge_rewards").html("");
             for(var d in data['goals'])
